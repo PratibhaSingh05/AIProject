0a1161d642b1b3631ac2fad315ea02a0d0e2d52f
PratibhaSingh05 <pratibhasingh1jp@gmail.com>
2025-08-16 13:42:26 +0530

    Update CORS settings for frontend-backend connection

--- ai-interview-assistant/server/server.js
+++ ai-interview-assistant/server/server.js
@@ -4,7 +4,11 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://ai-project-ten-peach.vercel.app/"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
